30a6c1510ace4f5ca9be90ede6f2932d50f05fe5
abiduzz420 <abiduzair420@gmail.com>
2019-04-01 13:36:51 +0530

    added support for binary data

--- cat.hs
+++ cat.hs
@@ -2,7 +2,7 @@ module Main where
 
 -- cat command
 -- reads files from cmdline or takes input from stdin
-
+import qualified Data.ByteString.Char8 as L
 import Control.Monad (when)
 import Control.Exception (IOException, try)
 import Data.Either (isLeft)
@@ -11,19 +11,19 @@ import System.IO (stderr, hPutStrLn)
 import System.Exit (exitFailure)
 
 type Argument = String
-type FileContent = String
+type FileContent = L.ByteString
 
 collect :: [Argument] -> IO [Either IOException FileContent]
-collect = mapM $ try . readFile
+collect = mapM $ try . L.readFile
 
 display :: [Either IOException FileContent] -> IO()
-display []    = getContents >>= putStr
+display []    = L.getContents >>= L.putStr
 display files = do
         mapM toConsole files
         when (any isLeft files) exitFailure
     where
         toConsole (Left exception) = hPutStrLn stderr $ show exception
-        toConsole (Right content)  = putStrLn content
+        toConsole (Right content)  = L.putStrLn content
         
 main :: IO()
 main = getArgs >>= collect >>= display
